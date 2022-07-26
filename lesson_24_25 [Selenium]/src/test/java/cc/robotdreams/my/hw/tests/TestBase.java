99c04edc3881c7d15200a8a7e09f5c78d51bd943
alexandr-sviderskiy <alexandr.sviderskiy@forcepoint.com>
2022-07-26 10:54:04 +0300

    rename module

--- lesson_24_25 [Selenium]/src/test/java/cc/robotdreams/my/hw/tests/TestBase.java
+++ lesson_24_25 [Selenium]/src/test/java/cc/robotdreams/my/hw/tests/TestBase.java
@@ -2,7 +2,9 @@ package cc.robotdreams.my.hw.tests;
 
 import cc.robotdreams.my.hw.appmanager.ApplicationManager;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 /**
  * Created by asvidersky on 7/11/2022.
@@ -11,12 +13,12 @@ import org.testng.annotations.BeforeMethod;
 public class TestBase {
     protected final ApplicationManager app = new ApplicationManager();
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeSuite(alwaysRun = true)
     public void setUp() {
         app.init();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
